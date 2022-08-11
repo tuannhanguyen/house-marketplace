94c17dfffedee58ac6f42947ce1899b9e92fccf7
tuannhan <tuannhan6@gmail.com>
2022-08-11 21:24:35 +0700

    Navbar,...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Explore from './pages/Explore';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      My App
+      <Router>
+        <Routes>
+          <Route path='/explore' element={<Explore/>}></Route>
+          <Route path='/offers' element={<Offers/>}></Route>
+          <Route path='/profile' element={<Profile/>}></Route>
+          <Route path='/sign-in' element={<SignIn/>}></Route>
+          <Route path='/sign-up' element={<SignUp/>}></Route>
+          <Route path='/forgot-password' element={<ForgotPassword/>}></Route>
+        </Routes>
+        <Navbar/>
+      </Router>
     </>
   );
 }
